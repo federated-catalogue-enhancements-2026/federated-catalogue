b2a14a1cabe05ffde7195469491f32971bf3a846
saackef <fabian.saacke@msg.group>
2026-03-06 09:20:31 +0100

    feat(CAT-FR-AC-01): introduce fine-granular permissions for RBAC

--- fc-service-server/src/main/java/eu/xfsc/fc/server/util/CommonConstants.java
+++ fc-service-server/src/main/java/eu/xfsc/fc/server/util/CommonConstants.java
@@ -1,9 +1,10 @@
 package eu.xfsc.fc.server.util;
 
 /**
- * Roles constant class.
+ * Roles and permissions constant class.
  */
 public final class CommonConstants {
+  // Legacy composite roles
   public static final String SD_ADMIN_ROLE = "Ro-SD-A";
   public static final String CATALOGUE_ADMIN_ROLE = "Ro-MU-CA";
   public static final String PARTICIPANT_ADMIN_ROLE = "Ro-MU-A";
@@ -12,4 +13,25 @@ public final class CommonConstants {
   public static final String CATALOGUE_ADMIN_ROLE_WITH_PREFIX = PREFIX + CATALOGUE_ADMIN_ROLE;
   public static final String PARTICIPANT_ADMIN_ROLE_WITH_PREFIX = PREFIX + PARTICIPANT_ADMIN_ROLE;
   public static final String PARTICIPANT_USER_ADMIN_ROLE_WITH_PREFIX = PREFIX + PARTICIPANT_USER_ADMIN_ROLE;
+
+  // Fine-grained permission roles
+  public static final String ASSET_CREATE = "ASSET_CREATE";
+  public static final String ASSET_READ = "ASSET_READ";
+  public static final String ASSET_UPDATE = "ASSET_UPDATE";
+  public static final String ASSET_DELETE = "ASSET_DELETE";
+  public static final String SCHEMA_CREATE = "SCHEMA_CREATE";
+  public static final String SCHEMA_READ = "SCHEMA_READ";
+  public static final String SCHEMA_UPDATE = "SCHEMA_UPDATE";
+  public static final String SCHEMA_DELETE = "SCHEMA_DELETE";
+  public static final String QUERY_EXECUTE = "QUERY_EXECUTE";
+  public static final String ADMIN_ALL = "ADMIN_ALL";
+
+  // Prefixed permission roles (for @WithMockJwtAuth authorities)
+  public static final String ASSET_CREATE_WITH_PREFIX = PREFIX + ASSET_CREATE;
+  public static final String ASSET_READ_WITH_PREFIX = PREFIX + ASSET_READ;
+  public static final String ASSET_UPDATE_WITH_PREFIX = PREFIX + ASSET_UPDATE;
+  public static final String ASSET_DELETE_WITH_PREFIX = PREFIX + ASSET_DELETE;
+  public static final String SCHEMA_CREATE_WITH_PREFIX = PREFIX + SCHEMA_CREATE;
+  public static final String SCHEMA_READ_WITH_PREFIX = PREFIX + SCHEMA_READ;
+  public static final String ADMIN_ALL_WITH_PREFIX = PREFIX + ADMIN_ALL;
 }
