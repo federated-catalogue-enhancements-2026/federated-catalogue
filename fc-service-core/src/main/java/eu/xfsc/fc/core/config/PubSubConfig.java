ead11a1d1dc09d24c1282229fc180b0cf21d6a00
saackef <fabian.saacke@msg.group>
2026-04-17 08:24:05 +0200

    feat(CAT-FR-SF-03): fix build

--- fc-service-core/src/main/java/eu/xfsc/fc/core/config/PubSubConfig.java
+++ fc-service-core/src/main/java/eu/xfsc/fc/core/config/PubSubConfig.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import eu.xfsc.fc.api.generated.model.AssetStatus;
+import eu.xfsc.fc.core.pojo.AssetMetadata;
+import eu.xfsc.fc.core.pojo.CredentialVerificationResult;
 import eu.xfsc.fc.core.service.pubsub.AssetPublisher;
 import eu.xfsc.fc.core.service.pubsub.AssetSubscriber;
 import eu.xfsc.fc.core.service.pubsub.ces.CesAssetPublisherImpl;
@@ -23,37 +26,48 @@ public class PubSubConfig {
     
     @Bean
     public AssetPublisher getAssetPublisher() {
-    	AssetPublisher pub = null;
-    	switch (pubImpl) {
-			//case "basf": 
-			//	pub = new BasfAssetPublisherImpl();
-			//	break;
-	    	case "ces":
-	    		pub = new CesAssetPublisherImpl();
-	    		break;
-			case "nats": 
-				pub = new NatsAssetPublisherImpl();
-				break;
-    	}
+    	AssetPublisher pub = switch (pubImpl) {
+            //case "basf":
+            //	pub = new BasfAssetPublisherImpl();
+            //	break;
+            case "ces" -> new CesAssetPublisherImpl();
+            case "nats" -> new NatsAssetPublisherImpl();
+            default -> new AssetPublisher() {
+                @Override
+                public boolean isTransactional() {
+                    return false;
+                }
+
+                @Override
+                public void setTransactional(boolean t) {
+                }
+
+                @Override
+                public boolean publish(AssetMetadata m, CredentialVerificationResult r) {
+                    return true;
+                }
+
+                @Override
+                public boolean publish(String hash, AssetEvent event, AssetStatus status) {
+                    return true;
+                }
+            };
+        };
     	log.debug("getAssetPublisher; returning {} for impl {}", pub, pubImpl);
     	return pub;
     }
 
     @Bean
     public AssetSubscriber getAssetSubscriber() {
-    	AssetSubscriber sub = null;
-    	switch (subImpl) {
-    		//case "basf": 
-    		//	sub = new BasfAssetSubscriberImpl();
-    		//	break;
-    	    case "ces":
-    	    	sub = new CesAssetSubscriberImpl();
-    	    	break;
-    		case "nats": 
-    			sub = new NatsAssetSubscriberImpl();
-    			break;
-    	}
-    	log.debug("getAssetSubscriber; returning {} for impl {}", sub, subImpl);
+    	AssetSubscriber sub = switch (subImpl) {
+            //case "basf":
+            //	sub = new BasfAssetSubscriberImpl();
+            //	break;
+            case "ces" -> new CesAssetSubscriberImpl();
+            case "nats" -> new NatsAssetSubscriberImpl();
+            default -> null;
+        };
+        log.debug("getAssetSubscriber; returning {} for impl {}", sub, subImpl);
     	return sub;
     }
         
