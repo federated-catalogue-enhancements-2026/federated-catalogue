8c21fe12ad4cb94cee7b4f2fce0c77f787b02c19
Eric Nowak <eric.nowak@msg.group>
2026-04-21 22:13:08 +0200

    correct equals contract

--- fc-service-core/src/main/java/eu/xfsc/fc/core/pojo/RdfClaim.java
+++ fc-service-core/src/main/java/eu/xfsc/fc/core/pojo/RdfClaim.java
@@ -82,18 +82,16 @@ public class RdfClaim {
 
   @Override
   public boolean equals(Object obj) {
-    if (this == obj)
-      return true;
-    if (obj == null)
-      return false;
-    if (getClass() != obj.getClass())
-      return false;
-    RdfClaim other = (RdfClaim) obj;
-    return Objects.equals(object, other.object) && Objects.equals(predicate, other.predicate)
-        && Objects.equals(subject, other.subject);
+      if (this == obj)
+          return true;
+      if (!(obj instanceof RdfClaim other))
+          return false;
+      return Objects.equals(object, other.object)
+              && Objects.equals(predicate, other.predicate)
+              && Objects.equals(subject, other.subject);
   }
 
-  @Override
+    @Override
   public String toString() {
     return "Claim[" + subject + " " + predicate + " " + object + "]";
   }
