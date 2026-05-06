ac041aab437f8b78d0fa28d254bd13babafc4b31
Eric Nowak <eric.nowak@msg.group>
2026-05-06 16:51:10 +0200

    add trust framework bundle loader

---
+++ fc-service-core/src/main/java/eu/xfsc/fc/core/config/TrustFrameworkRegistryConfig.java
@@ -0,0 +1,22 @@
+package eu.xfsc.fc.core.config;
+
+import eu.xfsc.fc.core.service.trustframework.TrustFrameworkBundleLoader;
+import eu.xfsc.fc.core.service.trustframework.TrustFrameworkRegistry;
+import eu.xfsc.fc.core.service.trustframework.TrustFrameworkRegistryImpl;
+
+import java.io.IOException;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Exposes a {@link TrustFrameworkRegistry} bean populated from classpath bundles at boot.
+ */
+@Configuration
+public class TrustFrameworkRegistryConfig {
+
+  @Bean
+  public TrustFrameworkRegistry trustFrameworkRegistry() throws IOException {
+    return new TrustFrameworkRegistryImpl(new TrustFrameworkBundleLoader().loadFromClasspath());
+  }
+}
